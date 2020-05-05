5db09e56c4364ed5532afa9b66609bb9372abf3c
Dimitri Ter-Saakov <terdimitri@gmail.com>
2020-05-05 11:53:46 -0400

    rename complete to done

--- task_utils.py
+++ task_utils.py
@@ -11,30 +11,30 @@ def add_task(name, parent_task='./', **kwargs):
     for key in kwargs:
         with open(os.path.join(location, key.upper()), 'w') as file:
             file.write(kwargs[key])
-    os.mknod(os.path.join(location, 'COMPLETED'))
+    os.mknod(os.path.join(location, 'DONE'))
 
 def complete_task(task):
-    """Mark task as completed. Lables the completed task with the completion
-    time and date
+    """Mark task as done. Lables the finished task with the completion
+    time and date.
     """
-    with open(os.path.join(task, 'COMPLETED'), 'w') as file:
+    with open(os.path.join(task, 'DONE'), 'w') as file:
         file.write(f'Completed on {datetime.datetime.now().isoformat()}')
 
 def subtasks(task):
-    """list all subtasks of the given task"""
+    """List all subtasks of the given task."""
     for subtask in os.listdir(task):
         if os.path.isdir(os.path.join(task, subtask)):
             yield os.path.join(task, subtask)
 
 def subtask_names(task):
-    """list the names of all subtasks of the given task"""
+    """List the names of all subtasks of the given task"""
     for subtask in subtasks(task):
         yield subtask.basename()
 
-def is_complete(task):
-    """Check if task is complete"""
+def is_done(task):
+    """Check if task is done"""
     try:
-        if os.path.getsize(os.path.join(task, 'COMPLETED')) > 0:
+        if os.path.getsize(os.path.join(task, 'DONE')) > 0:
             return True
     except OSError:
         pass
